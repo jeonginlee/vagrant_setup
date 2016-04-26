d00d9db4cfd3587a84b12941586fde4dd5258455
Jeongin Lee <jeonginl@umich.edu>
2016-04-26 15:22:26 -0400

    finished script

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # vagrant_setup
-Set up git, vim, tmux with config files for Vagrant + venv 
+Set up config files for Vagrant + venv 
+
+
+### Includes:
+* git and repo setup (URL in first command line argument)
+* vim, .vimrc, and Vundle
+* tmux
+
+
+## To run:
+'''
+
+'''
+
+
+### Additional set-up:
+1. :PluginInstall (inside of vim)
+
+### Possible updates
+* deteching command line arguments
+	* help flag
+	* error checking
+* pushing (without username and password)
+* scripting additional set-up
---
+++ set_up.sh
@@ -0,0 +1,32 @@
+echo " Git set-up ------------------------------------------"
+sudo apt-get install git
+
+wget https://raw.githubusercontent.com/jeonginlee/vagrant_setup/master/_gitignore --output-document=.gitignore
+
+# setting up for remote add
+git config --global user.name "Jeongin Lee"
+git config --global user.email jeonginl@umich.edu 
+
+git add .
+git commit -m "initial commit"
+
+git remote add origin $1	# passed in url for new repo 
+git remote -v	# Verify new remote
+
+# set up git configs
+# git push -u origin master
+
+
+echo " Vim set-up ------------------------------------------"
+sudo apt-get install vim
+
+wget https://raw.githubusercontent.com/jeonginlee/vim_tmux_setup/master/.vimrc --output-document=.vimrc
+mv .vimrc ~/
+
+# Vundle package for plugin management
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+
+echo " tmux set-up ----------------------------------------"
+sudo apt-get install tmux
+
